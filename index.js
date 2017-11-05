a8ad09d41701de2c9f113545fac6d88d1bd480ec
sunnode <m18205253820@163.com>
2017-11-05 21:34:41 +0800

    feat(content): add Content Type Parser

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const fastify = require('fastify')()
-
-// API Routes
+// api routes
 fastify.get('/', function (req, reply) {
   reply.send({ hello: 'world' })
 })
 
-// Server
+// json requests
+fastify.addContentTypeParser('application/json', (req, done) => {
+  jsoffParser(req, function (err, body) {
+    done(err || body)
+  })
+})
+
+// all requests
+fastify.addContentTypeParser('*', (req, done) => {
+  var data = ''
+  req.on('data', chunk => { data += chunk })
+  req.on('end', () => {
+    done(data)
+  })
+})
+
+// api server
 fastify.listen(3000, '127.0.0.1', (err) => {
   if (err) throw err
   console.log(`server has listening on port ${fastify.server.address().port}`)
